8be942dded8044dc614b1430fac720f9ea51cf9f
gaoxuerong <gaoxuerong123@gmail.com>
2019-11-30 23:03:04 +0800

    add 手写koa源码init

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const Koa = require('koa')
 const app = new Koa()
-app.use(async (ctx) => {
-    ctx.body = 'hello,雪荣,i ❤️ you'
+
+app.use((ctx,next) => {
+  ctx.body = `🚀`
+  console.log(ctx.req.url)
+  console.log(ctx.request.url)
+  console.log(ctx.request.req.url)
+  console.log(ctx.request.req === ctx.req)
+  // 当我去ctx上边取值时会去ctx.request上边取值，做了一层拦截；
 })
-app.listen('3000',() => {
-    console.log('3000 port start')
+app.listen(3000, 'localhost',() => {
+    console.log(`port 3000`)
 })
