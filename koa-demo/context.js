b4e003b24b6555e76457c3c6770a8b2df09c3079
gaoxuerong <gaoxuerong123@gmail.com>
2019-12-05 23:51:12 +0800

    .

--- koa-demo/context.js
+++ koa-demo/context.js
@@ -1,5 +1,9 @@
 const proto = {};
 function defineGetter(property, key) {
+  // Object.prototype.__defineGetter__()
+  // 已废弃；方法可以将一个函数绑定在当前对象的指定属性上，当那个属性的值被读取时，你所绑定的函数就会被调用。
+  // __defineSetter__同理
+  // koa源码用的是delegates这个npm包，这个npm包内部也是用的__defineGetter__，和__defineSetter__
   proto.__defineGetter__(key, function() {
     return this[property][key];
   });
