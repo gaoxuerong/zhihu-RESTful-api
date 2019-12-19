77615f0908c5c4b3968b14d036c8b0ae54bd8144
gaoxuerong <gaoxuerong123@gmail.com>
2019-12-20 07:46:02 +0800

    ajax

--- http/formdata-server.js
+++ http/formdata-server.js
@@ -1,14 +1,20 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 http.createServer(function(req, res){
     let arr = []
     req.on('data',function(data) {
         arr.push(data)
     })
+    if (req.url === '/ajax.html') {
+        console.log(path.resolve(__dirname)) // /Users/xueronggao/Desktop/zhihu-restful-api/http
+        return fs.createReadStream(path.resolve(__dirname,'ajax.html')).pipe(res)
+    }
     if (req.url === '/login') {
         req.on('end', function(){
             const str = Buffer.concat(arr).toString()
             const obj = require('querystring').parse(str)
-            res.end(obj)
+            res.end(JSON.stringify(obj))
             // const reg = /([^=&]*)=([^=&]*)/g
             // const obj = {}
             // str.replace(reg, function() {
