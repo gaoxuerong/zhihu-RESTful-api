30fc0a88f7e4e516a2c3788fa875bdaee8d10868
gaoxuerong <gaoxuerong123@gmail.com>
2019-12-27 20:35:19 +0800

    图片上传

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,14 @@
+const path = require('path')
 class HomeControler {
   index(ctx) {
     ctx.body = "<h1>jbnjgfnbfgjnjfgn</h1>";
+  }
+  upload(ctx) {
+    const file = ctx.request.files.file
+    const basename = path.basename(file.path)
+    ctx.body = {
+      url: `${ctx.origin}/uploads/${basename}`
+    }
   }
 }
 module.exports = new HomeControler()
--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const Koa = require('koa')
-const bodyparser = require('koa-bodyparser')
+const koaStatic = require('koa-static')
+const koaBody = require('koa-body')
 const error = require('koa-json-error')
 const parameter = require('koa-parameter')
 const mongoose = require('mongoose')
+const path =require('path')
 const app = new Koa()
 const routing = require('./routes')
 const { connectionStr } = require('./config')
 mongoose.connect(connectionStr, { useUnifiedTopology: true, useNewUrlParser: true }, () => console.log(`cloud database connect success!`))
 mongoose.connection.on('error', console.error)
+app.use(koaStatic(path.join(__dirname, 'public')))
 app.use(error({
   postFormat: (e, {stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest : {stack, ...rest}
 }))
-app.use(bodyparser()) // 解析请求体
+app.use(koaBody({
+  multipart: true, // 表示支持文件
+  formidable: {
+    uploadDir: path.join(__dirname, 'public/uploads'),
+    keepExtensions: true
+  }
+})) // 解析请求体
 app.use(parameter(app))
 routing(app)
 app.listen(3000, 'localhost',() => {
